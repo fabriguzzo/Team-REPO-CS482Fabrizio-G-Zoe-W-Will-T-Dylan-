97f37bd2efb1963c0b522694f656a4ccfa191940
dpmorales7 <112967576+dpmorales7@users.noreply.github.com>
2025-10-16 18:44:44 -0400

    Create team.js
    team class created

---
+++ Docs/Baseball/src/team.js
@@ -0,0 +1,102 @@
+export class Team{
+
+    constructor(name){
+    
+        this.name = name;
+    
+        this.team = new Map();
+    
+        this.team.set('Players', []);
+    
+    }
+    
+    //adds a player to players list in team map
+    
+    addPlayer(player){
+    
+        let players = this.team.get('Players');
+    
+        players.push(player);
+    
+    }
+    
+    //deletes a player from players list in team map
+    
+    deletePlayer(player){
+    
+        let players = this.team.get('Players');
+    
+        let index = players.indexOf(player);
+    
+        if (index !== -1) {
+    
+        players.splice(index, 1);
+    
+        }
+    
+    }
+    
+    //assigns a coach to the Coach in team map
+    
+    assignCoach(coach){
+    
+        this.team.set('Coach',coach);
+    
+    }
+    
+    //assigns manger to the Manager in team map
+    
+    assignManager(manager){
+    
+        this.team.set('Manager',manager);
+    
+    }
+    
+    //assigns a logo to the Logo in team map
+    
+    assignLogo(logo){
+    
+        this.team.set('Logo',logo);
+    
+    }
+    
+    //assigns a map record to Record in team map
+    
+    assignRecord(win,tie,loss){
+    
+        let record = new Map();
+    
+        record.set('Wins',win);
+    
+        record.set('Losses',loss);
+    
+        record.set('Ties',tie);
+    
+        this.team.set('Record',record);
+    
+    }
+    
+    //assigns a ordered list of teams to Schedule in team map
+    
+    assignSchedule(teams){
+    
+        this.team.set('Schedule',teams);
+    
+    }
+    //might need work
+
+    //returns a Map of key as the team name and value as team attributes
+
+    getTeam(){  
+
+        let team = new Map();
+    
+        team.set(this.name, this.team);
+    
+        return team;
+    
+    }
+    
+    
+    
+}
