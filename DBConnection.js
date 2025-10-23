cdd59b58496281465baa37c5bf3593defe10418b
Will Troisi <willtroisi@Wills-MacBook-Air.local>
2025-10-23 19:07:11 -0400

    DBConnection changed to testDB

--- DBConnection.js
+++ DBConnection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 exports.connect = function(where){
-    mongoose.connect(process.env.DB_URI)
+    mongoose.connect(process.env.TESTDB_URI)
         .then(() => console.log("Connected to MongoDB"))
         .catch(err => console.error("MongoDB connection error:", err));
     }
