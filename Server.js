afe25c4a61e9677c74cd80852bb02d8ac09c4f7d
Will Troisi <willtroisi@Wills-MacBook-Air.local>
2025-10-23 19:03:47 -0400

    Updated Server.js

--- Server.js
+++ Server.js
@@ -1,22 +1,11 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const path = require("path");
 require("dotenv").config();
 
-const app = express();
-app.use(express.json());
+const dbConnect = require("./DBConnection.js");
 
-mongoose.connect(process.env.DB_URI)
-  .then(() => console.log("Connected to MongoDB"))
-  .catch(err => console.error("MongoDB connection error:", err));
+const ExpressApp = require('./App.js');
 
-const filePath = path.join(__dirname, 'public_html', 'test.html');
+dbConnect.connect();
 
-app.get("/", (req, res) => {
-  res.sendFile(filePath);
-});
-
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server running on http://localhost:${process.env.PORT || 3000}`);
+ExpressApp.app.listen(process.env.PORT,process.env.HOSTNAME,function(){ // Listen to client requests in hostname:port
+    console.log(`Server Running on ${process.env.HOSTNAME}:${process.env.PORT}...`);
 });
