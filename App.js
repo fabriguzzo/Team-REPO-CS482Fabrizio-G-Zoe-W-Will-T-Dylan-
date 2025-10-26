c0e8873891d7c483c5fbb058d8717656aaa88300
Will Troisi <willtroisi@Wills-MacBook-Air.local>
2025-10-25 22:52:06 -0400

    App

--- App.js
+++ App.js
@@ -12,4 +12,9 @@ app.get("/", (req, res) => {
   res.sendFile(filePath);
 });
 
+//User
+const UserCont = require("./Docs/Baseball/Controller/UserController.js");
+app.post('/user',UserCont.postCreateOrUpdate); // register new user
+app.get('/user',UserCont.getAll);
+
 exports.app = app;
