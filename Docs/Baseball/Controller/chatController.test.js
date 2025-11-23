d7c47d7dabc21040e1ea4296de2df48cf5a509ab
Fabri <fabri@Fabris-MacBook-Air.local>
2025-11-23 17:12:00 -0500

    testing for chat DAO and Controller

---
+++ Docs/Baseball/Controller/chatController.test.js
@@ -0,0 +1,86 @@
+const controller = require("./chatController");
+const dao = require("../Model/chatDao");
+
+jest.mock('../Model/chatDao');
+
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
+//GetRec Messages
+test("Get Recent Messages", async () => {
+  const req = {};
+  const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
+
+  dao.getRecentMessages.mockResolvedValue([
+    { user: "Fabrizio", text: "Hi" }
+  ]);
+  await controller.getMessages(req, res);
+  expect(dao.getRecentMessages).toHaveBeenCalled();
+  expect(res.json).toHaveBeenCalledWith([{ user: "Fabrizio", text: "Hi" }]);
+});
+
+test("Get Recent Messages - Error", async () => {
+  const req = {};
+  const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
+
+  dao.getRecentMessages.mockRejectedValue(new Error("Fail"));
+  await controller.getMessages(req, res);
+  expect(res.status).toHaveBeenCalledWith(500);
+  expect(res.json).toHaveBeenCalledWith({ error: "Failed to load chat messages" });
+});
+
+//DeleteOe Message
+test("Delete One Message - Success", async () => {
+  const req = { params: { id: "123" } };
+  const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
+
+  dao.deleteMessage.mockResolvedValue(true);
+  await controller.deleteMessage(req, res);
+
+  expect(dao.deleteMessage).toHaveBeenCalledWith("123");
+  expect(res.status).toHaveBeenCalledWith(200);
+  expect(res.json).toHaveBeenCalledWith({ message: "Message Deleted" });
+});
+
+test("Delete One Message(Null)", async () => {
+  const req = { params: { id: "123" } };
+  const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
+
+  dao.deleteMessage.mockResolvedValue(null);
+  await controller.deleteMessage(req, res);
+  expect(res.status).toHaveBeenCalledWith(404);
+  expect(res.json).toHaveBeenCalledWith({ error: "No message found" });
+});
+
+test("Delete One Message(fail)", async () => {
+  const req = { params: { id: "123" } };
+  const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
+
+  dao.deleteMessage.mockRejectedValue(new Error("Fail"));
+  await controller.deleteMessage(req, res);
+  expect(res.status).toHaveBeenCalledWith(500);
+  expect(res.json).toHaveBeenCalledWith({ error: "Failed to Delete Message" });
+});
+
+//Delete all Messages
+test("Delete All Messages(Pass)", async () => {
+  const req = {};
+  const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
+
+  dao.deleteAllMessages.mockResolvedValue();
+  await controller.deleteAllMessages(req, res);
+  expect(dao.deleteAllMessages).toHaveBeenCalled();
+  expect(res.status).toHaveBeenCalledWith(200);
+  expect(res.json).toHaveBeenCalledWith({ message: "All messages are gone" });
+});
+
+test("Delete All Messages(Fail)", async () => {
+  const req = {};
+  const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
+  dao.deleteAllMessages.mockRejectedValue(new Error("Fail"));
+  await controller.deleteAllMessages(req, res);
+  expect(res.status).toHaveBeenCalledWith(500);
+  expect(res.json).toHaveBeenCalledWith({ error: "Failed to Delete all Messages" });
+});
+
---
+++ Docs/Baseball/Model/chatDao.test.js
@@ -0,0 +1,52 @@
+const chatDao = require('./chatDao');
+const dbConnect = require("../../../DBConnection.js");
+require("dotenv").config();
+
+beforeAll(async () => {
+    await dbConnect.connect(test);
+});
+afterAll(async () => {
+    await dbConnect.disconnect();
+});
+afterEach(async () => {
+    await chatDao.deleteAllMessages();
+});
+
+test("should create a chat message", async () => {
+    const msg = {
+        user: "Fabrizio",
+        text: "WASUP"
+    };
+
+    const created = await chatDao.createMessage(msg);
+    expect(created).toBeDefined();
+    expect(created.user).toBe("Fabrizio");
+    expect(created.text).toBe("WASUP");
+});
+
+test("should read recent messages", async () => {
+    await chatDao.createMessage({ user: "A", text: "Test A" });
+    await chatDao.createMessage({ user: "B", text: "Test B" });
+
+    const messages = await chatDao.getRecentMessages();
+    expect(messages.length).toBe(2);
+    expect(messages[0].text).toBe("Test A");
+    expect(messages[1].text).toBe("Test B");
+});
+
+test("should delete one message by id", async () => {
+    const msg = await chatDao.createMessage({ user: "X", text: "To delete" });
+    const deleted = await chatDao.deleteMessage(msg._id);
+    const found = await chatDao.getRecentMessages();
+    expect(deleted._id.toString()).toBe(msg._id.toString());
+    expect(found.length).toBe(0);
+});
+
+test("should delete all messages", async () => {
+    await chatDao.createMessage({ user: "FAB", text: "Msg1" });
+    await chatDao.createMessage({ user: "WILL", text: "Msg2" });
+    await chatDao.deleteAllMessages();
+    const messages = await chatDao.getRecentMessages();
+    expect(messages.length).toBe(0);
+});
+
