90365e9683b8ceaad4db799b86e7cf701ca6638f
Fabri <fabri@Fabris-MacBook-Air.local>
2025-11-23 16:06:00 -0500

    Delete functionality in chat box

--- Docs/Baseball/Controller/chatController.js
+++ Docs/Baseball/Controller/chatController.js
@@ -9,3 +9,31 @@ exports.getMessages = async function (req, res) {
         res.status(500).json({ error: "Failed to load chat messages" });
     }
 };
+
+//DeleteOne
+exports.deleteMessage = async function (req, res) {
+    try {
+        const id = req.params.id;
+        const deleted = await chatDao.deleteMessage(id);
+
+        if (!deleted) {
+            return res.status(404).json({ error: "No message found" });
+        }
+        res.status(200).json({ message: "Message Deleted" });
+    } catch (err) {
+        console.error("Error dmessage:", err);
+        res.status(500).json({ error: "Failed to Delete Message" });
+    }
+};
+
+//DeleteALL
+exports.deleteAllMessages = async function (req, res) {
+    try {
+        await chatDao.deleteAllMessages();
+        res.status(200).json({ message: "All messages are gone" });
+    } catch (err) {
+        console.error("Error da messages:", err);
+        res.status(500).json({ error: "Failed to Delete all Messages" });
+    }
+};
+
