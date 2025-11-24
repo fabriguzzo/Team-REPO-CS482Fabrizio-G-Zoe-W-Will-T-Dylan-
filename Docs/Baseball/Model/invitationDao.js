bd21714b8c4dcd9d79e39333dbdd8a57719530e9
dpmorales7 <112967576+dpmorales7@users.noreply.github.com>
2025-11-23 23:47:02 -0500

    Update invitationDao.js #38 #39 #40 #41

--- Docs/Baseball/Model/invitationDao.js
+++ Docs/Baseball/Model/invitationDao.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+
 const invitationSchema = new mongoose.Schema({
     recipient: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     sender: { type: String, required: true }, // username string
