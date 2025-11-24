a2edaaac9026df2376e98fc37475d5ada14f93d3
dylanmorales <dpmorales@loyola.edu>
2025-11-23 23:21:52 -0500

    Add invitation routes to app js  #38 #39 #40 #41

--- App.js
+++ App.js
@@ -1,31 +1,35 @@
 const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
-const multer = require('multer');
-const fs = require('fs');
+const multer = require("multer");
+const fs = require("fs");
 const session = require("express-session");
 
 const dao = require("./Docs/Baseball/Model/teamDao");
- 
+
 const teamController = require("./Docs/Baseball/Controller/teamController");
 const userController = require("./Docs/Baseball/Controller/UserController");
-const gameController = require('./Docs/Baseball/Controller/gameController.js');
+const gameController = require("./Docs/Baseball/Controller/gameController.js");
 const chatController = require("./Docs/Baseball/Controller/chatController");
+const invitationController = require("./Docs/Baseball/Controller/invitationController");
 
 const app = express();
+
+/* -------------------- SESSION -------------------- */
 const sessionM = session({
     secret: "yourSecretKey",
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
 });
-
 app.use(sessionM);
 
+/* -------------------- MIDDLEWARE -------------------- */
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public_html")));
 
+/* -------------------- PAGE ROUTES -------------------- */
 const testPath = path.join(__dirname, "public_html", "test.html");
 const addTeamPath = path.join(__dirname, "public_html", "teamMaker.html");
 const addUserPath = path.join(__dirname, "public_html", "userMaker.html");
@@ -37,18 +41,17 @@ const deleteUserPath = path.join(__dirname, "public_html", "deleteUser.html");
 const viewUserPath = path.join(__dirname, "public_html", "userView.html");
 const viewGamesPath = path.join(__dirname, "public_html", "viewGames.html");
 const updateUserPath = path.join(__dirname, "public_html", "updateUser.html");
-const chatPath = path.join(__dirname, "public_html", "chat.html" );
+const chatPath = path.join(__dirname, "public_html", "chat.html");
 const schedulePath = path.join(__dirname, "public_html", "schedule.html");
 const ratingsPath = path.join(__dirname, "public_html", "ratings.html");
 const viewRatingsPath = path.join(__dirname, "public_html", "viewRatings.html");
-
 
 app.get("/", (req, res) => res.sendFile(testPath));
 app.get("/addTeam", (req, res) => res.sendFile(addTeamPath));
 app.get("/addUser", (req, res) => res.sendFile(addUserPath));
 app.get("/login", (req, res) => res.sendFile(loginPath));
 app.get("/signup", (req, res) => res.sendFile(addUserPath));
-app.get('/guest', (req, res) => res.redirect('/'));
+app.get("/guest", (req, res) => res.redirect('/'));
 app.get("/addChild", (req, res) => res.sendFile(addChildPath));
 app.get("/addGame", (req, res) => res.sendFile(addGamePath));
 app.get("/viewTeams", (req, res) => res.sendFile(addTeamsPath));
@@ -61,76 +64,87 @@ app.get("/schedule", (req, res) => res.sendFile(schedulePath));
 app.get("/ratings", (req, res) => res.sendFile(ratingsPath));
 app.get("/viewRatings", (req, res) => res.sendFile(viewRatingsPath));
 
-//create a folder to store logos for teams
+/* -------------------- FILE UPLOAD: LOGOS -------------------- */
 const uploadDir = path.join(__dirname, 'public_html', 'uploads');
 if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir);
+    fs.mkdirSync(uploadDir);
 }
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, uploadDir);
+        cb(null, uploadDir);
     },
     filename: function (req, file, cb) {
-      const uniqueName = Date.now() + '-' + file.originalname;
-      cb(null, uniqueName);
+        cb(null, Date.now() + '-' + file.originalname);
     }
 });
-
 const upload = multer({ storage });
 
-
-//  the API routes for team (connected to controller)
+/* -------------------- TEAM ROUTES -------------------- */
 app.get("/teams", teamController.getAll);
 app.get("/teams/:id", teamController.getOne);
 app.get("/teams/name/:name", teamController.getByName);
+
 app.post("/teams", upload.single('logo'), teamController.createOrUpdate);
+
 app.delete("/teams/:id", teamController.deleteOne);
 app.delete("/teams", teamController.deleteAll);
-//app.use('/uploads', express.static(path.join(__dirname, 'public_html', 'uploads')));
+
 app.get("/teams/:id/logo", async (req, res) => {
     try {
-      const team = await dao.read(req.params.id);
-      if (team && team.logo && team.logo.data) {
-        res.contentType(team.logo.contentType);
-        res.send(team.logo.data);
-      } else {
-        res.status(404).send("Logo not found");
-      }
+        const team = await dao.read(req.params.id);
+        if (team && team.logo && team.logo.data) {
+            res.contentType(team.logo.contentType);
+            res.send(team.logo.data);
+        } else {
+            res.status(404).send("Logo not found");
+        }
     } catch (err) {
-      console.error("Error serving logo:", err);
-      res.status(500).send("Server error");
+        console.error("Error serving logo:", err);
+        res.status(500).send("Server error");
     }
 });
 
-//User
+/* -------------------- USER ROUTES -------------------- */
 app.get("/users", userController.getAll);
 app.get("/users/:id", userController.getOneUser);
 app.get("/users/name/:name", userController.getByName);
+
 app.post("/users", userController.createOrUpdate);
-app.post('/login', userController.login);
-app.delete("/users/:id", userController.deleteOne);
-app.delete("/users", userController.deleteAll);
-app.post('/users/:parentId/children', userController.addChild);
 app.put("/users/:username", userController.createOrUpdate);
+
+app.post("/login", userController.login);
 app.post("/logout", userController.logout);
 
+app.delete("/users/:id", userController.deleteOne);
+app.delete("/users", userController.deleteAll);
 
-//Game
-app.post('/game', gameController.create);
-app.get('/game', gameController.getAll);
-app.get('/game/:id', gameController.getOne);
-app.put('/game/:id/score', gameController.updateScore);
-app.put('/game/:id/finish', gameController.finishGame);
-app.put('/game/:id/type', gameController.updateMatchType);
-app.delete('/game/:id', gameController.deleteOne);
-app.delete('/game', gameController.deleteAll);
+app.post("/users/:parentId/children", userController.addChild);
 
-//chat
+/* -------------------- GAME ROUTES -------------------- */
+app.post("/game", gameController.create);
+app.get("/game", gameController.getAll);
+app.get("/game/:id", gameController.getOne);
+app.put("/game/:id/score", gameController.updateScore);
+app.put("/game/:id/finish", gameController.finishGame);
+app.put("/game/:id/type", gameController.updateMatchType);
+app.delete("/game/:id", gameController.deleteOne);
+app.delete("/game", gameController.deleteAll);
+
+/* -------------------- CHAT ROUTES -------------------- */
 app.get("/api/chat", chatController.getMessages);
 app.delete("/api/chat/:id", chatController.deleteMessage);
 app.delete("/api/chat", chatController.deleteAllMessages);
 
+/* -------------------- INVITATION ROUTES -------------------- */
+app.post("/invitations", invitationController.send);
+app.get("/invitations", invitationController.getAll);
+app.get("/invitations/user/:userId", invitationController.getByRecipient);
+app.get("/invitations/team/:teamId", invitationController.getByTeam);
+app.put("/invitations/:id/accept", invitationController.accept);
+app.put("/invitations/:id/decline", invitationController.decline);
+app.delete("/invitations/:id", invitationController.delete);
+
+/* -------------------- EXPORT -------------------- */
 exports.sessionM = sessionM;
 exports.app = app;
-
